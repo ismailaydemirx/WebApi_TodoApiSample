9da8d083e76874140d5c0739da06f7b08d5b00ed
Ismail Aydemir <theangelo112@gmail.com>
2023-08-10 11:03:36 +0300

    EF Core | Get 1

--- WebApi_TodoApiSample/Controllers/TodoController.cs
+++ WebApi_TodoApiSample/Controllers/TodoController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using WebApi_TodoApiSample.Entities;
 
 namespace WebApi_TodoApiSample.Controllers
@@ -15,5 +18,35 @@ namespace WebApi_TodoApiSample.Controllers
         {
             _db= context;
         }
+
+        [HttpGet("generate-fakedata")]
+        public IActionResult GenerateFakeData()
+        {
+            if (_db.Todos.Any())
+            {
+                return Ok("Veri tabanındaki todos tablosunda zaten örnek veri mevcuttur");
+            }
+            for (int i = 0; i < 50; i++)
+            {
+                Todo todo = new Todo
+                {
+                    Text = MFramework.Services.FakeData.TextData.GetSentence(),
+                    IsCompleted = MFramework.Services.FakeData.BooleanData.GetBoolean(),
+                    Description = MFramework.Services.FakeData.TextData.GetSentences(2),
+                };
+                _db.Todos.Add(todo);
+            }
+
+            _db.SaveChanges();
+            return Ok("Ok");
+        }
+
+        [HttpGet("list")]
+        [ProducesResponseType((int)HttpStatusCode.OK,Type =typeof(List<Todo>))] // ProduceResponseType OK durumunda bize typeof Todo'yu list olarak döndürecek.
+        public IActionResult List()
+        {
+            return Ok(_db.Todos.ToList());
+        }
+
     }
 }
